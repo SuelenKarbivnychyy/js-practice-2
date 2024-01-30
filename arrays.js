cbb333787db86798b07f463418d573adb32e3cb4
Suelen Karbivnychyy <suelenmatosr@gmail.com>
2024-01-29 19:57:42 -0500

    started developing Javascript functions

--- arrays.js
+++ arrays.js
@@ -2,15 +2,38 @@
 
 // 1. printIndices
 function printIndices(items) {
-  // Replace this with your code
+
+  for (const item in items) {
+    console.log(items[item], item);
+  }  
+  
 }
+// printIndices(["apple", "berry", "cherry"])
+
+
 
 // 2. everyOtherItem
 function everyOtherItem(items) {
-  // Replace this with your code
+  
+  let result = [];
+
+  for (const item in items) {
+    if (item %2 === 0) {
+      result.push(items[item])
+    }
+  }
+
+  console.log(result)
 }
+
+// console.log(everyOtherItem(["apple", 2, "love", "monkey", 85, 42, 79]))
+
 
 // 3. smallestNItems
 function smallestNItems(items, n) {
-  // Replace this with your code
+  // console.log(`list items ${items}`);
+
+  // let result = [];  
 }
+
+
