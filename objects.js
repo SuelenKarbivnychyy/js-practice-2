406d4017b6012bdef9c7cc7a886656432fd82ce9
Suelen Karbivnychyy <suelenmatosr@gmail.com>
2024-02-02 18:32:47 -0500

    Implemented funtions on objects.js

--- objects.js
+++ objects.js
@@ -1,11 +1,56 @@
 'use strict';
 
-// 1. countWords
+/**
+ * Return a dictionary of each word and the no. of times they appear.
+
+    You can assume that `phrase` does not contain any punctuation and that
+    each word is separated with a space.
+
+ * @param {*} phrase (str): A string of space-separated words
+ * @returns dict: {word: number of times word appears in `phrase`}
+ */
+
 function countWords(phrase) {
-  // Replace this with your code
+
+  let wordCounts = {};
+
+  for (const word of phrase.split(' ')) {
+    if (wordCounts.hasOwnProperty(word)) {
+      wordCounts[word] += 1;
+    } else {
+      wordCounts[word] = 1;
+    }
+  }
+  
+  return wordCounts;
+  
 }
 
-// 2. getMelonsAtPrice
+// console.log(countWords('The quick brown fox jumps over the lazy dog ick brown fox jumps'));
+
+
+
+
+/**
+ * Return the list of melons that cost `price`.
+ * @param {*} price : A number
+ * @returns list: List of melons that cost `price`
+            None: If there are no melons that cost `price`
+ */
 function getMelonsAtPrice(price) {
-  // Replace this with your code
+
+  const melon_prices = {
+    2.50: ['Cantaloupe', 'Honeydew'],
+    2.95: ['Watermelon'],
+    3.25: ['Musk', 'Crenshaw'],
+    14.25: ['Christmas']
+  };
+
+  if (melon_prices.hasOwnProperty(price)) {
+    return melon_prices[price].sort();
+  } else {
+    return 'none';
+  }
 }
+
+// console.log(getMelonsAtPrice(3.25))
